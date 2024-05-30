fb190435d80f3dab6743eda039f69fbd137aaa48
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-30 16:26:04 -0300

    data and logic to results page

--- lib/page/results.dart
+++ lib/page/results.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResultsPage extends StatefulWidget {
   final String bill;
@@ -19,6 +20,10 @@ class ResultsPage extends StatefulWidget {
 
 class ResultsPageState extends State<ResultsPage> {
   String dividedAmount = '';
+
+  TextStyle infoStyle = GoogleFonts.montserrat(
+      fontSize: 18, color: Colors.black, fontWeight: FontWeight.w700);
+
   @override
   void initState() {
     super.initState();
@@ -38,6 +43,114 @@ class ResultsPageState extends State<ResultsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 40),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Result',
+                style: GoogleFonts.montserrat(
+                  fontSize: 40,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Colors.yellow,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Equally Divided',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          '\$ $dividedAmount',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Friends', style: infoStyle),
+                            Text('Tax', style: infoStyle),
+                            Text('Tip', style: infoStyle),
+                          ],
+                        ),
+                        const SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(widget.friends.toStringAsFixed(0),
+                                style: infoStyle),
+                            Text('${widget.tax} %', style: infoStyle),
+                            Text(widget.tip.toStringAsFixed(0),
+                                style: infoStyle),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Everybody should pay $dividedAmount',
+              style: GoogleFonts.montserrat(
+                  fontSize: 30,
+                  color: Colors.green,
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 100,
+              height: 50,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.deepOrange,
+                ),
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  'Calculate again ?',
+                  style: GoogleFonts.montserrat(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
