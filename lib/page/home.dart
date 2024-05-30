5803e9775623e58746e066c21fa4ccb24231c70d
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-30 14:04:35 -0300

    upper container of HomePage

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -9,11 +9,14 @@ class BillSplit extends StatefulWidget {
 }
 
 class _BillSplitState extends State<BillSplit> {
+  TextStyle infoStyle = GoogleFonts.montserrat(
+      fontSize: 18, color: Colors.black, fontWeight: FontWeight.w700);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-         margin: const EdgeInsets.only(left: 20, right: 20),
+        margin: const EdgeInsets.only(left: 20, right: 20),
         child: Column(
           children: [
             Container(
@@ -33,6 +36,57 @@ class _BillSplitState extends State<BillSplit> {
               height: 100,
               decoration: const BoxDecoration(
                 color: Colors.yellow,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Total',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          '34',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Friends', style: infoStyle),
+                            Text('Tax', style: infoStyle),
+                            Text('Tip', style: infoStyle),
+                          ],
+                        ),
+                        const SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('10', style: infoStyle),
+                            Text('14%', style: infoStyle),
+                            Text('15', style: infoStyle),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             )
           ],
